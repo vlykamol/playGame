897454bf5e20a78d159956f005592755e47ddccf
vlykamol <vlykamol@outlook.com>
2021-07-30 03:53:54 +0530

    check win loose button added, wrong style added

--- sudoku.js
+++ sudoku.js
@@ -5,88 +5,193 @@ let turn = false;
 
 tds.forEach((td) => {
   td.addEventListener('click', (e) => {
+    const node = document.getElementById(`${e.target.id}`);
     e.target.innerText = prompt();
-    checkRow(e)
-    checkColl(e)
-    checkSqr(e);
+    checkRow(node.parentNode);
+    checkColl(node.id);
+    checkSqr(node.id);
   })
 })
 
-const ch = (children2, inn, id) =>{
-  const node = document.getElementById(`${id}`);
-  const num = parseInt(node.textContent);
-  console.log(node);
-  children2.sort();
-  for(let i = 0; i < children2.length; i++){
-    if(children2[i] === children2[i+1]){
-      console.log('wrong in ', inn, children2[i]);
-      if(children2[i] == num){
-        node.classList.add('wrong')
-      }
+const ifWin = () =>{
+  console.log('checking...');
+  let check1;
+  let check2 = false;
+  let check3 = false;
+  for(let i = 1; i <= 73; i += 9){
+    check1 = checkRow(document.getElementById(i).parentNode);
+    console.log(check1);
+    if(check1 === false) break;
+  }
+  for(let i = 1; i <= 9; i++){
+    check2 = checkColl(i);
+    console.log(check2);
+    if(check2 === false) break;
+  }
+  for(let i = 1; i <= 7; i += 3){
+    for(let j = i; j <= i + 54; j += 27){
+      check3 = checkSqr(j);
+      console.log(check3);
+      if(check3 === false) break;
     }
+  }
+  if(check1 && check2 && check3){
+    console.log('you win the game');
+  }else{
+    console.log('check again');
   }
 }
 
+const ch = (children2, inn) => {
+  console.log(inn,'-----------');
+  let n1 = 0;
+  let n2 = 0;
+  let n3 = 0;
+  let n4 = 0;
+  let n5 = 0;
+  let n6 = 0;
+  let n7 = 0;
+  let n8 = 0;
+  let n9 = 0;
+  let a = 0;
+  children2.forEach((td) => {
+    const num = td.innerText;
+    if(num === '1') {
+      n1++;
+      if(n1>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '2') {
+      n2++;
+      if(n2>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '3') {
+      n3++;
+      if(n3>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '4') {
+      n4++;
+      if(n4>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '5') {
+      n5++;
+      if(n5>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '6') {
+      n6++;
+      if(n6>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '7') {
+      n7++;
+      if(n7>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '8') {
+      n8++;
+      if(n8>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+    if(num === '9') {
+      n9++;
+      if(n9>1) {
+        td.classList.add(`${inn}`);
+      }else{
+        a++;
+        td.classList.remove(`${inn}`);
+      }
+    }
+  })
+  return a === 9 ? true : false;
+}
+
 const checkRow = (id) => {
-  const row = id.target.parentNode;
+  const row = id;
+  console.log('check row', row);
   const children = [...row.children];
-  var children2 = [];
-  children.forEach((num) => {
-    children2.push(parseInt(num.innerText))
-  });
-  ch(children2, 'row', id.target.id);
+  console.log(ch(children, 'row'));
+  return ch(children, 'row');
 }
 
 const checkColl = (id) => {
-  id = id.target.id;
+  const node = document.getElementById(`${id}`);
+  console.log('check coll', node);
   num = id % 9;
   num = num === 0 ? 9 : num;
   children2 = [];
   for(let i = num; i < 82; i += 9){
-    const first = document.getElementById(`${i}`).textContent;
-    children2.push(parseInt(first));
+    const first = document.getElementById(`${i}`);
+    children2.push(first);
   }
-  ch(children2, 'coll', id);
+  return ch(children2, 'coll');
 }
 
 
 const checkSqr = (id) =>{
-  id = id.target.id;
+  const node = document.getElementById(`${id}`);
+  console.log('check sqr', node);
   num = id % 9;
   num = num === 0 ? 9 : num;
 
   if(num <= 3){
     if(id <= 27){
-      // console.log('1st sqr');
-      getSqr(1, id);
+      return getSqr(1, id);
     } else if (id >= 55) {
-      // console.log('7th sqr');
-      getSqr(55, id);
+      return getSqr(55, id);
     } else {
-      // console.log('4th sqr');
-      getSqr(28, id);
+      return getSqr(28, id);
     }
   } else if (num >= 7) {
     if(id <= 27){
-      // console.log('3rd sqr');
-      getSqr(7, id);
+      return getSqr(7, id);
     } else if (id >= 55) {
-      // console.log('9th sqr');
-      getSqr(61, id);
+      return getSqr(61, id);
     } else {
-      // console.log('6th sqr');
-      getSqr(34, id);
+      return getSqr(34, id);
     }
   } else {
     if(id <= 27){
-      // console.log('2nd sqr');
-      getSqr(4, id);
+      return getSqr(4, id);
     } else if (id >= 55) {
-      // console.log('8th sqr');
-      getSqr(58, id);
+      return getSqr(58, id);
     } else {
-      // console.log('5th sqr');
-      getSqr(31, id);
+      return getSqr(31, id);
     } 
   }
 }
@@ -94,12 +199,10 @@ const checkSqr = (id) =>{
 const getSqr = (id, num) => {
   children2 = []
   for(let i = id; i <= id+2; i++){
-
-    const first = document.getElementById(`${i}`).textContent;
-    const second = document.getElementById(`${i + 9}`).textContent;
-    const third = document.getElementById(`${i + 18}`).textContent;
-    children2.push(parseInt(first), parseInt(second), parseInt(third));
-
+    const first = document.getElementById(`${i}`);
+    const second = document.getElementById(`${i + 9}`);
+    const third = document.getElementById(`${i + 18}`);
+    children2.push(first, second, third);
   }
-  ch(children2, 'sqr', num);
+  return ch(children2, 'sqr');
 }
