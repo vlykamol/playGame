4bb31b91887f4553b3cf0bfbe9342c13f9fb97ba
vlykamol <vlykamol@outlook.com>
2021-08-11 19:13:45 +0530

    all working snake game

--- snake/snake.js
+++ snake/snake.js
@@ -1,16 +1,30 @@
 console.log('snake is running');
 const main_container = document.getElementById('snake');
 const score = document.getElementById('score')
+const highScore = document.getElementById('highscore')
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d')
+
+const popup = document.querySelector('.popup');
+const msg = document.querySelector('.msg');
+const newGameBtn = document.getElementById('newGameBtn')
 
 canvas.width = main_container.clientWidth;
 canvas.height = main_container.clientHeight;
 
+
+window.addEventListener('resize' , () => {
+  console.log('window is resized');
+  console.log(main_container, main_container.clientWidth, main_container.clientHeight);
+  canvas.width = main_container.clientWidth;
+  canvas.height = main_container.clientHeight;
+})
+
 let sc = 0
+let hsc = 0;
 let speed = 100; 
 let dir = '';
-let snakeTail = [[30, 10], [20, 10], [10, 10]]
+let snakeTail = [[60, 10], [50, 10], [40, 10], [30, 10], [20, 10], [10, 10]]
 
 globalThis.x = snakeTail[0][0];
 globalThis.y = snakeTail[0][1];
@@ -31,15 +45,25 @@ const food = () => {
 
 const updateScore = () =>{
   score.innerText = `score : ${sc}`
+  highScore.innerText = `high score : ${hsc}` 
 }
 
+var collison;
+var animate;
+globalThis.isHit = false;
 
-window.addEventListener('resize' , () => {
-  console.log('window is resized');
-  console.log(main_container, main_container.clientWidth, main_container.clientHeight);
-  canvas.width = main_container.clientWidth;
-  canvas.height = main_container.clientHeight;
-})
+const checkCollison = () => {
+  console.log('checking any collison');
+  for(let i = 2; i < snakeTail.length; i++){
+    if(snakeTail[i][0] === snakeTail[1][0] && snakeTail[i][1] === snakeTail[1][1]){
+      clearTimeout(animate);
+      isHit = true
+      if(sc > hsc) hsc = sc;
+      stop();
+    }
+  }
+  collison = isHit === false ? setTimeout(checkCollison, speed) : clearTimeout(collison)
+}
 
 const snake = (x, y) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -57,23 +81,10 @@ const snake = (x, y) => {
     globalThis.y = 0
   }
   for(let i = 0; i < snakeTail.length; i++){
-    ctx.fillStyle = i===0? 'red' : 'black';
+    ctx.fillStyle = i === 0 ? 'red' : 'black';
     ctx.fillRect(snakeTail[i][0], snakeTail[i][1], 10, 10)
   }
   food();
-}
-
-var animate;
-
-
-const checkCollison = () => {
-  for(let i = 2; i < snakeTail.length; i++){
-    if(snakeTail[i][0] === snakeTail[1][0] && snakeTail[i][1] === snakeTail[1][1]){
-      console.log('hit', snakeTail[i]);
-      return true
-    }
-  }
-  return false
 }
 
 window.addEventListener('keydown', (e) => {
@@ -102,11 +113,12 @@ window.addEventListener('keydown', (e) => {
     }
   }
   if(e.key === 'Escape'){
-    clearTimeout(animate)
+    pause();
   }
 })
 
 const moveRight = () => {
+  // console.log('rig');
   dir = 'r';
   x += dx;
   // snakeTail[0][0] += dx;
@@ -118,14 +130,11 @@ const moveRight = () => {
     snakeTail.push([fx, fy])
     randomPos()
   }
-  if(checkCollison()) {
-    clearTimeout(animate)
-  }
-  else{
-    animate = setTimeout(moveRight, speed);
-  }
+  animate = setTimeout(moveRight, speed);
 }
+
 const moveLeft = () => {
+  // console.log('lee');
   dir = 'l';
   x -= dx;
   // snakeTail[0][0] -= dx;
@@ -137,14 +146,11 @@ const moveLeft = () => {
     snakeTail.push([fx, fy])
     randomPos()
   }
-  if(checkCollison()) {
-    clearTimeout(animate)
-  }
-  else{
-    animate = setTimeout(moveLeft, speed);
-  }
+  animate = setTimeout(moveLeft, speed);
 }
+
 const moveUP = () => {
+  // console.log('upp');
   dir = 'u';
   y -= dy;
   // snakeTail[0][1] -= dy;
@@ -156,14 +162,11 @@ const moveUP = () => {
     snakeTail.push([fx, fy])
     randomPos()
   }
-  if(checkCollison()) {
-    clearTimeout(animate)
-  }
-  else{
-    animate = setTimeout(moveUP, speed);
-  }
+  animate = setTimeout(moveUP, speed);
 }
+
 const moveDown = () => {
+  // console.log('dow');
   dir = 'd';
   y += dy;
   // snakeTail[0][1] += dy;
@@ -175,16 +178,66 @@ const moveDown = () => {
     snakeTail.push([fx, fy])
     randomPos()
   }
-  if(checkCollison()) {
-    clearTimeout(animate)
-  }
-  else{
-    animate = setTimeout(moveDown, speed);
-  }
+  animate = setTimeout(moveDown, speed);
 }
-
 
 const updateSnake = (snakeTail) => {
   snakeTail.unshift([x,y])
   snakeTail.pop()
 }
+
+
+const init = () => {
+  console.log('initializing....');
+  sc = 0
+  speed = 100; 
+  dir = '';
+  snakeTail = [[60, 10], [50, 10], [40, 10], [30, 10], [20, 10], [10, 10]]
+  globalThis.x = snakeTail[0][0];
+  globalThis.y = snakeTail[0][1];
+  globalThis.fx = 50;
+  globalThis.fy = 80;
+  globalThis.isHit = false
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  for(let i = 0; i < snakeTail.length; i++){
+    ctx.fillStyle = i === 0 ? 'red' : 'black';
+    ctx.fillRect(snakeTail[i][0], snakeTail[i][1], 10, 10)
+  }
+  food();
+  checkCollison();
+  updateScore();
+}
+
+const stop = () => {
+  console.log('stop');
+  msg.innerText = 'game Over'
+  clearTimeout(animate);
+  clearTimeout(collison);
+  popup.style.display = 'flex';
+  updateScore();
+}
+
+const resume = document.createElement('button');
+resume.innerText = 'resume';
+
+
+const pause = () => {
+  console.log('pause');
+  msg.innerText = 'menu'
+  clearTimeout(animate);
+  clearTimeout(collison);
+  popup.append(resume);
+  popup.style.display = 'flex';
+}
+
+resume.addEventListener('click', (e) => {
+  popup.style.display = 'none';
+  collison = setTimeout(checkCollison, speed);
+  resume.remove();
+})
+
+
+newGameBtn.addEventListener('click', (e) => {
+  init();
+  popup.style.display = 'none'
+})
